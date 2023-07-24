6164288a26a1a63e9ff3671336c9ec38b01d33e8
Alexander Ratushnyi <olexandr.ratushnyi@gmail.com>
2023-07-24 18:29:01 +0300

    update

--- src/views/HomeView.jsx
+++ src/views/HomeView.jsx
@@ -1,15 +1,11 @@
 import { Container, Image } from './HomeView.styled';
 import imagePhone from '../images/image-homepage.jpg';
-import { Suspense } from 'react';
-import Loader from 'components/Loader/Loader';
 
 export default function HomeView() {
   return (
-    <Suspense fallback={<Loader />}>
-      <Container>
-        Your personal Phonebook!
-        <Image src={imagePhone}></Image>
-      </Container>
-    </Suspense>
+    <Container>
+      Your personal Phonebook!
+      <Image src={imagePhone}></Image>
+    </Container>
   );
 }

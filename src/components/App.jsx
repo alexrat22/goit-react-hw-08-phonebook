d6180762d0de432dbb6249db05323a5947ef813d
Alexander Ratushnyi <olexandr.ratushnyi@gmail.com>
2023-07-26 08:23:27 +0300

    mentor corrections

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,12 +4,14 @@ import { fetchCurrentUser } from 'redux/operations';
 import { Routes, Route } from 'react-router-dom';
 import PrivateRoute from './PrivateRoute';
 import PublicRoute from './PublicRoute';
-import AppBar from './AppBar/AppBar';
-import HomeView from '../views/HomeView';
-import RegisterView from 'views/RegisterView';
-import LoginView from '../views/LoginView';
-import ContactsView from '../views/ContactsView';
 import { getIsFetchingCurrent } from 'redux/selectors';
+import AppBar from './AppBar/AppBar';
+import { lazy, Suspense } from 'react';
+import Loader from './Loader/Loader';
+const HomeView = lazy(() => import('../views/HomeView'));
+const RegisterView = lazy(() => import('views/RegisterView'));
+const LoginView = lazy(() => import('../views/LoginView'));
+const ContactsView = lazy(() => import('../views/ContactsView'));
 
 export default function App() {
   const dispatch = useDispatch();
@@ -22,35 +24,37 @@ export default function App() {
   return (
     <>
       {!isFetchingCurrentUser && (
-        <Routes>
-          <Route path="/" element={<AppBar />}>
-            <Route index element={<HomeView />} />
-            <Route
-              path="register"
-              element={
-                <PublicRoute>
-                  <RegisterView />
-                </PublicRoute>
-              }
-            />
-            <Route
-              path="login"
-              element={
-                <PublicRoute>
-                  <LoginView />
-                </PublicRoute>
-              }
-            />
-            <Route
-              path="contacts"
-              element={
-                <PrivateRoute>
-                  <ContactsView />
-                </PrivateRoute>
-              }
-            />
-          </Route>
-        </Routes>
+        <Suspense fallback={<Loader />}>
+          <Routes>
+            <Route path="/" element={<AppBar />}>
+              <Route index element={<HomeView />} />
+              <Route
+                path="register"
+                element={
+                  <PublicRoute>
+                    <RegisterView />
+                  </PublicRoute>
+                }
+              />
+              <Route
+                path="login"
+                element={
+                  <PublicRoute>
+                    <LoginView />
+                  </PublicRoute>
+                }
+              />
+              <Route
+                path="contacts"
+                element={
+                  <PrivateRoute>
+                    <ContactsView />
+                  </PrivateRoute>
+                }
+              />
+            </Route>
+          </Routes>
+        </Suspense>
       )}
     </>
   );
